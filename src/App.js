912e971f05d16cda67906460d5b8ae8cebc54446
rlysam <bellosamuelb@gmail.com>
2023-02-25 19:52:18 +0800

    Sprite is now Visible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-//import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-			 hello
-    </div>
-  );
+	const [pokemonImageUrl, setPokemonImageUrl] = useState("");
+
+	const getPokemonImageUrl = async () => {
+		try {
+			const response = await axios.get(
+				"https://pokeapi.co/api/v2/pokemon/" +
+					Math.floor(Math.random() * 898) +
+					1
+			);
+			const pokemon = response.data;
+			setPokemonImageUrl(pokemon.sprites.front_default);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	useEffect(() => {
+		getPokemonImageUrl();
+	}, []);
+
+	return (
+		<div className="App">
+			hello
+			<img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" } alt="Pokemon" />
+		</div>
+	);
 }
 
 export default App;
